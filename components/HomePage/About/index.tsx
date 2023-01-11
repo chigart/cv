1937dbfe3d520c8747ec2108a847f7d08e6e4ba3
Anton Malkov <chigart@gmail.com>
2023-01-11 10:26:36 -0300

    fixed text

--- components/HomePage/About/index.tsx
+++ components/HomePage/About/index.tsx
@@ -5,15 +5,15 @@ import shuffleArray from '@/lib/shuffleArray';
 const rawText = [
   'I have 3.5 years of real experience in programming, including 1.5 years of full-time company jobs and 2 years of ' +
   'freelancing and part-time. I started with Pascal at 10 years old, created a simple arcade game that won a student competition. ' +
-  'At 19 I received a bachelor degree in Computer Science. All these years writing code was a more of a hobby for me. 2 years ago, after ' +
+  'At 19 I received a bachelor degree in Computer Science. All these years writing code was more of a hobby for me. 2 years ago, after ' +
   'trying out a couple of different careers: photographer, filmmaker, event organizer, I finally started programming full-time as a ' +
   'frontend developer.',
-  'At my first company after just 3 month of work i achieved a role of a Main Frontend Developer in two big projects. ' +
+  'At my first company job after just 3 month I achieved a role of a Main Frontend Developer in two big projects. ' +
   'And i have been in this role since. I was responsible for all frontend development, code-review of every piece of frontend code. ' +
-  'In all of my projects I had a couple junior frontend developers to lead',
-  'I am a digital nomad, location independent. I can consider relocation to Spain or Portugal, but currently working remotely from one of ' +
-  'these countries: Turkey, Thailand, Georgia. My citizenship is Russian, registered as an individual entrepreneur in Georgia. I am fluent ' +
-  'in English, native in Russian, speak some spanish.'
+  'In all of my projects I had a couple junior frontend developers to lead.',
+  'I am a digital nomad, location independent. Can consider relocation to Spain or Portugal, but currently working remotely from one of ' +
+  'these countries: Turkey, Thailand, Georgia. My citizenship is russian, registered as an individual entrepreneur in Georgia. I am fluent ' +
+  'in english, native in russian, speak some spanish.'
 ];
 
 const About = (): JSX.Element => {
@@ -49,23 +49,20 @@ const About = (): JSX.Element => {
 
   return (
     <>
-      <h2>
-        About me
-      </h2>
-
       <div
         className={styles.wrapper}
         onMouseLeave={() => setSectionRearranging(-1)}
       >
-        <p onMouseEnter={() => setSectionRearranging(0)}>
-          { text[0].join('') }
-        </p>
-        <p onMouseEnter={() => setSectionRearranging(1)}>
-          { text[1].join('') }
-        </p>
-        <p onMouseEnter={() => setSectionRearranging(2)}>
-          { text[2].join('') }
-        </p>
+        { rawText?.map((p, index) => {
+          return (
+            <p
+              key={index}
+              onMouseEnter={() => setSectionRearranging(index)}
+            >
+              { text[index].join('') }
+            </p>
+          );
+        }) }
       </div>
     </>
   );
